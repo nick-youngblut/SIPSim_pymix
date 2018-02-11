5f757cff88088869112c7e776d77b9410c5c351a
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2018-02-11 17:29:35 +0100

    added conda_env include dir to include_dir

--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@
 from distutils.core import setup, Extension
 import distutils.sysconfig
 import numpy.distutils.misc_util
+import os
 
 # Get the arrayobject.h(numpy) and python.h(python) header file paths:
 include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs()
 include_dirs.insert(0, distutils.sysconfig.get_python_inc())
+## including conda include/ dir if found
+try:
+    conda_include = os.path.join(os.environ['CONDA_PREFIX'], 'include')
+    include_dirs.insert(0, conda_include)
+except KeyError:
+    pass
 
+# c module that requires gsl
 module1 = Extension('_C_mixextend',
                     ['SIPSim_pymix/C_mixextend.c'],
                     include_dirs = include_dirs,
                     libraries = ['gsl', 'gslcblas' ,'m'])
 
-
+# setup
 setup(name='SIPSim_pymix',
       version="0.1.0",
       description='Python mixture package dependency for SIPSim',
