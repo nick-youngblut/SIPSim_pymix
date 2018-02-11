558676733cbe2e7efa086872551beb73124f8685
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2018-02-11 15:43:56 +0100

    removed setuptools from setup.py

--- README.rst
+++ README.rst
@@ -8,5 +8,6 @@ Author: Nick Youngblut
 Original Author: Benjamin Georgi <georgi@molgen.mpg.de>
 
 See pymix.org for information on the package.
-Note that this package is just a modified version of pymix,
+
+> Note that this package is just a modified version of pymix,
 which is required for the SIPSim package.
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
 import distutils.sysconfig
 import numpy.distutils.misc_util
-from setuptools import setup, find_packages
 
 # Get the arrayobject.h(numpy) and python.h(python) header file paths:
 include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs()
 include_dirs.insert(0, distutils.sysconfig.get_python_inc())
+
+module1 = Extension('_C_mixextend',
+                    ['SIPSim_pymix/C_mixextend.c'],
+                    include_dirs = include_dirs,
+                    libraries = ['gsl', 'gslcblas' ,'m'])
+
 
 setup(name='SIPSim_pymix',
       version="0.1.0",
@@ -16,15 +21,8 @@ setup(name='SIPSim_pymix',
       author_email="nyoungb2@gmail.com",
       url ="https://github.com/nick-youngblut/SIPSim_pymix",
       license='GNU General Public License v2.0',
-      packages = find_packages(),
+      packages = ['SIPSim_pymix'],
       package_dir = {'SIPSim_pymix' : 'SIPSim_pymix'},
-      ext_modules = [Extension('_C_mixextend',
-                               ['SIPSim_pymix/C_mixextend.c'],
-                               include_dirs = include_dirs,
-                               libraries = ['gsl', 'gslcblas' ,'m'],
-                               )
-                     ],
-      requires = [
-          'numpy',
-      ]
+      ext_modules = [module1],
+      requires = ['numpy']
      )
